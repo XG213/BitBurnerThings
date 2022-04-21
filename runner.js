87b26b9c2b1293484d009b2ad4f8d708b1c01b48
XG213 <101302347+XG213@users.noreply.github.com>
2022-04-21 22:02:21 +1200

    Update runner.js

--- runner.js
+++ runner.js
@@ -1,14 +1,16 @@
-import { spider } from "scripts/spider.js";
+import { spider } from "./spider.js";
 export async function main(ns) {
 	var serverList = []
 	var list = await spider(ns)
 	for (let i = 0; i <= list.length - 1; i++) {
 		if (ns.hasRootAccess(list[i])) {
-			serverList.push(list[i])
+			if (!(list[i] == "home")) {
+				serverList.push(list[i])
+			}
 		}
 	}
 	for (let i = 0; i <= serverList.length - 1; i++) {
 		ns.print(i)
-		ns.run("scripts/hack_script.js", ns.args[0], serverList[i])
+		ns.run("./hack_script.js", ns.args[0], serverList[i])
 	}
 }
