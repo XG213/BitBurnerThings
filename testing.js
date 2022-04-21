78ad8983507b38cbd9872dcb5a54902e00bd9a09
XG213 <101302347+XG213@users.noreply.github.com>
2022-04-21 19:20:50 +1200

    Delete testing.js

--- testing.js
+++
@@ -1,8 +0,0 @@
-export async function main(ns) {
-    ns.print(ns.args[0]);
-    while (true) {
-        await ns.grow(ns.args[0])
-        await ns.hack(ns.args[0])
-        await ns.weaken(ns.args[0])
-    }
-}
